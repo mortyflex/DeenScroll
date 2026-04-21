e6a764e8ca6dc1f74ee6d08c21a0137180330646
Mohamed Azz <mortyflex@gmail.com>
2026-04-21 11:34:34 +0200

    ✨ feat(app): enhance tab navigation by adding icons and titles for all screens

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,9 +1,54 @@
 import { Tabs } from "expo-router";
+import { Entypo, Feather } from "@expo/vector-icons";
 
 export default function TabsLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{}} />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <Entypo name="home" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="friends"
+        options={{
+          title: "Friends",
+          tabBarIcon: ({ color }) => (
+            <Feather name="users" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="newPost"
+        options={{
+          title: "New Post",
+          tabBarIcon: ({ color }) => (
+            <Feather name="plus-square" size={28} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="inbox"
+        options={{
+          title: "Inbox",
+          tabBarIcon: ({ color }) => (
+            <Feather name="inbox" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color }) => (
+            <Feather name="user" size={24} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
